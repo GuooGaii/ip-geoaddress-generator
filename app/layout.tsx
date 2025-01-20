7a71d12ebd059471ea1fee5508dcf2ea2800d206
GuooGaii <guoogaii@gmail.com>
2025-01-20 21:34:51 +0800

    feat: 改用geist字体；优化seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,25 @@
 import "@radix-ui/themes/styles.css";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "真实地址生成器",
   description: "基于IP地址生成真实地址",
+  keywords:
+    "IP地址, 地址生成器, 地理位置, IP定位, IP查询, IP地址查询, 地理位置查询, 位置生成器, 地址查询工具, IP工具, 地址定位, 位置信息",
+  authors: [{ name: "GuooGaii" }],
+  openGraph: {
+    title: "真实地址生成器",
+    description: "基于IP地址生成真实地址",
+    type: "website",
+    locale: "zh_CN",
+    siteName: "真实地址生成器",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh">
-      <body className={inter.className}>
+      <body className={GeistSans.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
